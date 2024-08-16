1ac66aceee7f27233b772ad8b7f0018c102ed64f
Foxerxl <113719788+Foxerxl@users.noreply.github.com>
2024-08-16 13:03:05 -0300

    Update Teacher.cs

--- Teacher.cs
+++ Teacher.cs
@@ -1,1 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using Unity.VisualScripting;
 
+public class Teacher : MonoBehaviour
+{
+
+}
